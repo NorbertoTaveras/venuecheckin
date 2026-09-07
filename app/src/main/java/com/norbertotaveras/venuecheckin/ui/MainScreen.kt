2bbd2000e393830b455b17c6d1fa0ecc659ee4b5
Norberto Taveras <NorbertoTaveras@users.noreply.github.com>
2026-09-06 23:52:09 -0400

    Fix staturs bar padding

--- app/src/main/java/com/norbertotaveras/venuecheckin/ui/MainScreen.kt
+++ app/src/main/java/com/norbertotaveras/venuecheckin/ui/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -26,6 +27,7 @@ fun MainScreen(
 ) {
     Column(
         modifier = Modifier
+            .statusBarsPadding()
             .fillMaxSize()
             .padding(24.dp)
     ) {
